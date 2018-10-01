4acfee694fa780d360ecaa49eadf6803f02e9359
Petros Kyriakou <9pitops@gmail.com>
2018-10-01 23:06:47 +0300

    initial commit

---
+++ src/pages/2018-10-1-halloween/index.md
@@ -0,0 +1,10 @@
+---
+path: "/happy-halloween"
+date: 2018-10-01T03:15:59.165Z
+title: "Happy Halloween"
+tags: ['holiday', 'fall', 'pumpkin']
+excerpt: "Great Pumpkin, or Greatest Pumpkin?" 
+---
+
+# YOOOOOOOO
+It was a clear black night, the stars were brimming with light like a christmas tree.
---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = ({data}) => {
+  const {edges: posts} = data.allMarkdownRemark
+  return (
+    <Layout>
+      {posts.map(({ node: post}) => {
+        const { frontmatter } = post
+
+        return (
+          <div>
+            <h2>
+              <Link to={frontmatter.path}>
+                {frontmatter.title}
+              </Link>
+            </h2>
+            <p>{frontmatter.date}</p>
+            <p>{frontmatter.excerpt}</p>
+
+            <ul>
+              {post.frontmatter.tags.map(tag => {
+                return (
+                  <li>
+                    <Link to={`/tags/${tag}`}>
+                      {tag}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+            tags
+            excerpt
+          }
+        }
+      }
+    }  
+  }
+`
+export default IndexPage
